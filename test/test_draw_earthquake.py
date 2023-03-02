1a0025307c46b1036708c988c308d18db0acbaa6
Miewliie <miewliie@gmail.com>
2023-03-02 14:24:53 -0800

    Add test data - draw earthquake points on vancouver map and output file.

--- test/test_draw_earthquake.py
+++ test/test_draw_earthquake.py
@@ -1,6 +1,7 @@
 import unittest
 from draw.draw_earthquake import convert_lon_to_x_pixel, convert_lat_to_y_pixel, draw_earthquake_points
 from os import path
+import json
 
 
 class TestDrawEarthquake(unittest.TestCase):
@@ -20,12 +21,16 @@ class TestDrawEarthquake(unittest.TestCase):
         self.assertEqual(640.0, y_pixel)
 
     def test_draw_earthquake_points(self):
-        output_path = "./outputs/vancouver_earthquake_map.png"
-        base_image_path = "./assets/vancouver_base_map.png"
-        earthquake_test_data = [{"lat": "xx", "long": "yy"}]
+        # TODO: cleanup this test by verify the draw of xy coordinate on white canvas.
+        #  To not testing the lib! like this
+        output_path = "./output_test/vancouver_earthquake_map.png"
+        base_image_path = "../assets/vancouver_base_map.png"
+
+        with open('./test_data/lat_lon_earthquake.json', "r", encoding="utf-8") as output_file:
+            earthquake_test_data = json.loads(output_file.read())
 
         draw_earthquake_points(base_image_path, output_path, earthquake_test_data)
-        self.assertTrue(path.exists('./outputs/vancouver_earthquake_map.png'),
+        self.assertTrue(path.exists('./output_test/vancouver_earthquake_map.png'),
                         'Vancouver earthquake map is not exist in output')
 
 
