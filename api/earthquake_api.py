9cc814f20fc6fa933d131ae3aa981d197b4b02f7
Miewliie <miewliie@gmail.com>
2023-03-02 16:59:55 -0800

    Update api query & end date will be 30 days from current day

--- api/earthquake_api.py
+++ api/earthquake_api.py
@@ -1,12 +1,14 @@
 import requests
 import json
+from datetime import date, timedelta
 
 
-START_DATE = '2023-02-01'
-END_DATE = '2023-03-24'
+START_DATE = date.today().isoformat()
+END_DATE = (date.today()+timedelta(days=30)).isoformat()
+# vancouver lat & long
 LATITUDE = '49.246292'
 LONGITUDE = '-123.116226'
-MAX_RADIUS_KM = '700'
+MAX_RADIUS_KM = '1000'
 EVENT_TYPE = 'earthquake'
 MIN_MAGNITUDE = '3.0'
 USGS_API = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
@@ -14,8 +16,10 @@ USGS_API = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
 
 def get_earthquakes():
     response = requests.get(USGS_API + '&starttime=' + START_DATE + '&endtime=' + END_DATE + '&latitude=' + LATITUDE +
-                            '&longitude=' + LONGITUDE + '&maxradiuskm=' + MAX_RADIUS_KM + '&eventtype=' + EVENT_TYPE + '&minmagnitude=' + MIN_MAGNITUDE)
+                            '&longitude=' + LONGITUDE + '&maxradiuskm=' + MAX_RADIUS_KM + '&eventtype=' + EVENT_TYPE +
+                            '&minmagnitude=' + MIN_MAGNITUDE)
     json_data = response.json()
+
     return json_data
 
 
