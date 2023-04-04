023e2a5309598e7d1750940abc4ea3b3e090d866
Miewliie <miewliie@gmail.com>
2023-04-04 14:13:47 -0700

    [eq] Convert UTC time into Vancouver (PST) time

--- earthquake/earthquake.py
+++ earthquake/earthquake.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import pytz
 
 TOP = 53.7827
 BOTTOM = 44.7827
@@ -43,20 +44,30 @@ def get_earthquake_title():
         y_latitude = obj["geometry"]["coordinates"][1]
 
         if RIGHT > x_longitude > LEFT and TOP > y_latitude > BOTTOM:
+
             second = properties["time"] / 1000
-            temp_datetime = datetime.fromtimestamp(second).replace(microsecond=0)
+            utc_datetime = datetime.utcfromtimestamp(second)
+
+            utc_timezone = pytz.timezone('UTC')
+            localized_utc_datetime = utc_timezone.localize(utc_datetime)
+
+            vancouver_timezone = pytz.timezone('America/Vancouver')
+
+            vancouver_datetime = localized_utc_datetime.astimezone(vancouver_timezone)
+            temp_datetime = vancouver_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
             title_dt.append(temp_title)
             title_dt.append(str(temp_datetime))
-            title_dt.append("UTC")
+            title_dt.append('Vancouver Time')
 
             str_title_dt = ' : '.join(title_dt)
             titles.append(str_title_dt)
 
     if len(titles) > 0:
+        titles.append("#Vancouver #VancouverEarthquake #Earthquake #EarthquakeVancouver")
         title = '\n'.join(titles)
         print("total title char: ", len(title))
-        print(title)
+        # print(title)
         return title
     else:
         title = titles
