32fd8ebef6df5141c7d5921a8294428d15ded36f
Miewliie <miewliie@gmail.com>
2023-05-02 18:58:46 -0700

    [storage] refactor variable name

--- vancouver_earthquake/storage/storage_manager.py
+++ vancouver_earthquake/storage/storage_manager.py
@@ -1,16 +1,14 @@
 import vancouver_earthquake.storage.data_handler as json_handler
 from vancouver_earthquake.core.earthquake import Earthquake
 
-old_path = "../outputs/old_earthquake.json"
 
-
-def read_earthquake_data() -> list[Earthquake]:
+def read_earthquake_data(old_eq_path: str) -> list[Earthquake]:
     """ Get old earthquake data from previous run. """
-    old_eq = json_handler.read_json(old_path)
+    old_eq = json_handler.read_json(file_path=old_eq_path)
     return json_handler.earthquake_encoder(old_eq)
 
 
-def write_earthquake_data(new_eq: list[Earthquake]):
+def write_earthquake_data(new_eq: list[Earthquake], old_path: str):
     """ Save latest earthquake data to file. """
-    earthquake_json = json_handler.earthquake_decoder(new_eq)
+    earthquake_json = json_handler.earthquake_decoder(earthquakes=new_eq)
     json_handler.write_json(earthquake_json, old_path)
